72f74fb30ac7df55502b9c641b23056e2a85bbfe
joethorley <joe@poissonconsulting.ca>
2015-12-17 11:40:14 -0800

    Updated documentation ferox data projection

--- R/data.R
+++ R/data.R
@@ -21,8 +21,8 @@
 #'   \item{Length}{The fork length of the fish at (re)capture (in mm) as an integer.}
 #'   \item{Mass}{The wet mass of the fish at (re)capture (in kg) as a numeric.}
 #'   \item{Age}{The scale age of the fish at (re)capture (in yr) as an integer.}
-#'   \item{Easting}{The easting (in km of projection EPSG:27700) as a numeric.}
-#'   \item{Northing}{The northing (in km of projection EPSG:27700) as a numeric.}
+#'   \item{Easting}{The easting (in km of projection EPSG:32630) as a numeric.}
+#'   \item{Northing}{The northing (in km of projection EPSG:32630) as a numeric.}
 #' }
 #' @seealso \code{\link{ranmrdata}}
 "ferox"
